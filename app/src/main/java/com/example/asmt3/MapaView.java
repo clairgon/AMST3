1a4ce9cd7a7b1ab3d36ca2361370f78bda310ffa
clairgon <clairgon@espol.edu.ec>
2021-06-10 16:51:11 -0500

    cambios a mi rama

--- app/src/main/java/com/example/asmt3/MapaView.java
+++ app/src/main/java/com/example/asmt3/MapaView.java
@@ -1,6 +1,5 @@
 package com.example.asmt3;
-
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MapaView extends AppCompatActivity {
